595c5098aa4cdada9429829a0e7ab69389f1527d
anugraha4 <96745531+anugraha4@users.noreply.github.com>
2022-12-06 23:54:52 +0530

    view all

--- Courseapp_backend/src/main/java/com/nest/Courseapp_backend/control/CourseController.java
+++ Courseapp_backend/src/main/java/com/nest/Courseapp_backend/control/CourseController.java
@@ -1,5 +1,6 @@
 package com.nest.Courseapp_backend.control;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +13,8 @@ public class CourseController {
         return "course added successfully";
     }
 
-
+    @GetMapping("/view")
+    public String ViewAll(){
+        return "View all courses";
+    }
 }
